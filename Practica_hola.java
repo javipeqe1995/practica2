c584b20290cbf8ec65d0152d9cd819afccfa1b08
javier pequeño <javi_peqe1995@hotmail.com>
2021-02-19 12:36:47 +0100

    Primera Version

---
+++ Practica_hola.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_hola;
+
+/**
+ *
+ * @author javi_
+ */
+public class Practica_hola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
