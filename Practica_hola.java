09d4b737a6dd7942a284162cec9d383e41f68820
javier pequeño <javi_peqe1995@hotmail.com>
2021-02-19 12:55:49 +0100

    segunda version

--- Practica_hola.java
+++ Practica_hola.java
@@ -16,6 +16,7 @@ public class Practica_hola {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("hola nueva modificacion introducida");
     }
     
 }
